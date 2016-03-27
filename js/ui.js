46945bb4730778e0fb04b3a9f8551e993673b6b8
Giancarlo <sakya_stelios@hotmail.com>
2016-03-27 13:58:02 -0500

    Corrected bug on export HTML

--- js/ui.js
+++ js/ui.js
@@ -295,10 +295,8 @@ ZenPen.ui = (function() {
 
 			case 'html':
 				header = "<h1>" + header + "</h1>";
-				body.replaceAll('<br\/?>[\s]*?\n?[\s]*', '</p>\n</p>')
-				console.log('First body: ', body);
-				body.replaceAll('<br>[\s]*?\n?[\s]*', '</p>\n</p>')
-				console.log('Second body: ', body);
+				body = body.replaceAll('<br\/?>[\s]*?\n?[\s]*', '</p>\n</p>')
+				body = body.replaceAll('<br>[\s]*?\n?[\s]*', '</p>\n</p>')
 				text = header + body;
 				text = text.replace(/\t/g, '');
 			break;
